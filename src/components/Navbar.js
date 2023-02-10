83e7d6c58f72619a6791940e8ebee01572ae0cd7
AdiSince2002 <81612690+AdiSince2002@users.noreply.github.com>
2023-02-11 01:03:58 +0530

    update navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./NavbarStyles.css";
+import {NavLink} from "react-router-dom";
  
 function Navbar1() {
   return ( 
     <>
       <Navbar sticky="top" expand="lg" variant="dark" className="top-container">
       <Container>
-        <Navbar.Brand id="h1" href="/">@adityakahanhai</Navbar.Brand>
+        <NavLink id="h1" to="/">@adityakahanhai</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto"> 
-            <Nav.Link className="ul-links" href="/">Home</Nav.Link>
-            <Nav.Link className="ul-links" href="/about">About</Nav.Link>
-            <Nav.Link className="ul-links" href="/project">Project</Nav.Link>
-            <Nav.Link className="ul-links" href="/contact">Contact</Nav.Link>
+            <NavLink className="ul-links" to="/">Home</NavLink>
+            <NavLink className="ul-links" to="/about">About</NavLink>
+            <NavLink className="ul-links" to="/project">Project</NavLink>
+            <NavLink className="ul-links" to="/contact">Contact</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
