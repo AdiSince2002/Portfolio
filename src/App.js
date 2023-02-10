a8ff3348b561141f76a1928b5b04558a63d6ac66
AdiSince2002 <81612690+AdiSince2002@users.noreply.github.com>
2023-02-11 01:04:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import { Routes, Route } from "react-router-dom";
  
 function App() {
   return ( 
-    <div> 
+    <> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>  
-    </div>  
+    </>  
   );  
 }
 
